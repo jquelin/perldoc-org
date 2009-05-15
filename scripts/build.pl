7f08eba5fc29da89b3bd7544ea3df8e145aba756
Gabor Szabo <szabgab@gmail.com>
2009-05-15 15:29:51 +0300

    split up the front-page generation to each language

--- scripts/build.pl
+++ scripts/build.pl
@@ -11,7 +11,7 @@ die if not -d 'out';
 
 foreach my $lang (keys %sites) {
 	my $outpath = lc $lang;
-	system "$^X scripts/build-perldoc-static.pl --output-path out/$outpath";
+	system "$^X scripts/build-perldoc-static.pl --language $outpath --output-path out/$outpath";
 	system "$^X scripts/build-perldoc-html.pl --output-path out/$outpath --input-path src/POD2-$lang-$sites{$lang}/$lang --cache cache/$lang.cache";
 }
 system "cp -r www out/www";
